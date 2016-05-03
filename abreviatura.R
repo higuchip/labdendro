cade05966401612d6de971479a6dc0be9a6605d4
higuchip <higuchip@gmail.com>
2016-05-03 00:34:17 -0300

    Update abreviatura.R

--- abreviatura.R
+++ abreviatura.R
@@ -1,3 +1,5 @@
+#Abreviando o nome científico das espécies
+
 teste<-read.csv("http://dl.dropbox.com/u/6511995/testeR.csv", header=T, row.names=1, sep=";") #para importar uma matriz com dados hipotéticos
 teste # o resultado deve ser uma matriz 5x5, sendo que as linhas correspondem às espécies e as colunas indicam as parcelas
 library(vegan) #a biblioteca vegan deve estar previamente instalada
