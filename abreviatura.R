19f2d6d6c0c410b5f23eebebb176405ec7625f64
higuchip <higuchip@gmail.com>
2016-05-08 13:43:31 -0300

    Update abreviatura.R

--- abreviatura.R
+++ abreviatura.R
@@ -1,4 +1,12 @@
-#Abreviando o nome científico das espécies
+#--------------------------------------------------------------------------------------------
+# Abreviando o nome científico de espécies usando a função make.cepnames da biblioteca vegan
+#
+# Pedro Higuchi
+# Departamento de Engenharia Florestal
+# Universidade do Estado de Santa Catarina
+# Contato: higuchip [at] gmail.com
+#
+#---------------------------------------------------------------------------------------------
 
 teste<-read.csv("http://dl.dropbox.com/u/6511995/testeR.csv", header=T, row.names=1, sep=";") #para importar uma matriz com dados hipotéticos
 teste # o resultado deve ser uma matriz 5x5, sendo que as linhas correspondem às espécies e as colunas indicam as parcelas
